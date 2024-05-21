57bf55865d613399a87cd35a392094cd59eb9a16
hojo <hojo@ic.ideacloud.co.jp>
2024-05-22 01:26:05 +0900

    alert削除

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -1336,7 +1336,6 @@ class UIRoot extends Component {
       }
     ];
     console.log("isModerator:"+isModerator);
-    alert("isModerator:"+isModerator);
 
     return (
       <MoreMenuContextProvider>
