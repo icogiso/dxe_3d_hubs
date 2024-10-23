6c6d953ddf1b3f1c701abc4397faa68624bd85d4
hojo <hojo@ic.ideacloud.co.jp>
2024-10-23 15:56:24 +0900

    一時的にリファラー制限削除

--- src/hub.js
+++ src/hub.js
@@ -274,18 +274,18 @@ import { loadLegacyRoomObjects } from "./utils/load-legacy-room-objects";
 import { loadSavedEntityStates } from "./utils/entity-state-utils";
 import { shouldUseNewLoader } from "./utils/bit-utils";
 
-export const customBackLink = "https://metapus.metabooth.jp/events/metapus/venue/user/login.php";
+// export const customBackLink = "https://metapus.metabooth.jp/events/metapus/venue/user/login.php";
 
-const referrer = document.referrer.split('?')[0];
-console.log("Current referrer (without query params):", referrer);
+// const referrer = document.referrer.split('?')[0];
+// console.log("Current referrer (without query params):", referrer);
 
-var mod_debug = localStorage.getItem('mod_debug');
-if (!mod_debug) {
-  if (!referrer || !referrer.includes("light3dauth")) {
-    alert("ルームへのアクセスは正規の方法でログインしてください");
-    window.location.href = customBackLink;
-  }
-}
+// var mod_debug = localStorage.getItem('mod_debug');
+// if (!mod_debug) {
+//   if (!referrer || !referrer.includes("light3dauth")) {
+//     alert("ルームへのアクセスは正規の方法でログインしてください");
+//     window.location.href = customBackLink;
+//   }
+// }
 
 const PHOENIX_RELIABLE_NAF = "phx-reliable";
 NAF.options.firstSyncSource = PHOENIX_RELIABLE_NAF;
